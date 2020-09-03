2d12e133d6698b55e5fa7a4f353c3b9fc26b4b15
233518 <233518@usn.no>
2020-09-03 14:14:19 +0200

    Hello world

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+const path = require("path");
+const http = require("http");
 const express = require("express");
-
+const port = 3000;
 const app = express();
+const publicPath = path.join(__dirname, "/public");
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.set("view engine", "pug");
+app.use(express.static(publicPath));
+
+app.get("/", async function (req, res) {
+  res.render("index", {
+    title: "Hello World",
+  });
+});
+
+app.listen(port, () => console.log("Example app listening on port 3000!"));
