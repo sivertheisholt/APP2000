f28e3a3d254cb1afd82ba7c5824fb85d517138b6
dyb0 <dybevik2@gmail.com>
2021-01-21 18:08:12 +0100

    fjernet ting + oppdatert brukerScheme

--- handling/brukerSchema.js
+++ handling/brukerSchema.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 
 //Vi lager et nytt schema
 const brukerSchema = new mongoose.Schema({
-    email: String,
-    password: String, //Denne skal da bli hashed + salt
-  })
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: true,
+    maxlength: 1024
+  }
+});
 
 module.exports = mongoose.model('bruker', brukerSchema); //ES6 Module
