17a15e2162504bf2cab6412da763770486a6bebb
233518 <233518@usn.no>
2021-04-05 13:51:07 +0200

    Gjorde movieHandler mer modulær

--- handling/movieHandler.js
+++ handling/movieHandler.js
@@ -2,31 +2,36 @@ const Film = require('../database/filmSchema');
 const logger = require("../logging/logger");
 const ValidationHandler = require("./ValidationHandler");
 
+function returnHandler(doc, err) {
+    if(err) {
+        logger.log({level: 'error', message: `There was en error when working with the database! Error: ${err}`});
+        return new ValidationHandler(false, 'Something unexpected happen when working with the database');
+    }
+    if(doc === null) {
+        logger.log({level: 'debug', message: `Return doc is null`});
+        return new ValidationHandler(false, 'Return doc is null');
+    }
+    logger.log({level: 'info', message: `Operation successfully completed`});
+    return new ValidationHandler(true, doc);
+}
+
 //Legger til film i databasen
 function addToDatabase(movie) {
     logger.log({level: 'debug', message: `Adding movie to database with id: ${movie.id}...`});
     delete movie.production_companies, movie.production_countries, movie.spoken_languages
     const film = new Film(movie);
-    return film.save().then((doc, err) => {
-        if(err) {
-            logger.log({level: 'error', message: `There was an error adding the movie to the database! Error: ${err}`});
-            return new ValidationHandler(false, 'Could not add movie to the database!');
-        }
-        logger.log({level: 'info', message: `Movie with id ${movie.id} was saved to the database`});
-        return new ValidationHandler(true, 'Movie was successfully saved to the database');
-    })
+    return film.save().then((doc, err) => returnHandler(doc, err));
 }
 
 //Sjekker om filmen eksisterer i databasen
 async function checkIfSaved(movieId) {
     logger.log({level: 'debug', message: `Checking if movie is already saved in database! MovieId: ${movieId} `});
-    const film = await Film.findOne({id: movieId});
-    if(film) {
-        logger.log({level: 'debug', message: `${movieId} is already in the database`});
-        return new ValidationHandler(true, `${movieId} is already in the database`);
-    }
-    logger.log({level: 'debug', message: `${movieId} is not in the database`});  
-    return new ValidationHandler(false, `Movie is not in the database`);
+    return Film.findOne({id: movieId}).then((doc, err) => returnHandler(doc, err));
 }
 
-module.exports = {addToDatabase, checkIfSaved}
+async function getMovieFromDatabaseById(movieId)  {
+    logger.log({level: 'debug', message: `Getting movie from database with id ${movieId}`});
+    return Film.findOne({id: movieId}).then((doc,err) => returnHandler(doc,err));
+}
+
+module.exports = {addToDatabase, checkIfSaved, getMovieFromDatabaseById}
