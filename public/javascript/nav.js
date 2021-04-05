0ae65b5f530b1909e70dba27919e9fe49efd9b83
dyb0 <dybevik2@gmail.com>
2021-04-05 16:06:41 +0200

    more language fix ish

--- public/javascript/nav.js
+++ public/javascript/nav.js
@@ -1,9 +1,5 @@
 let hamburger = document.querySelector('.hamburger');
 let menu = document.querySelector('.menu');
-let bod = document.querySelector('.container');
-let nav = document.querySelector('.lang');
-let select = document.querySelectorAll('.select li');
-let nor = document.getElementById('nor');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('isactive');
@@ -11,16 +7,3 @@ hamburger.addEventListener('click', function() {
 });
 
 
-
-const handleClick = (e) => {
-    select.forEach(node => {
-      node.classList.remove('uk-active');
-    });
-    var lang = e.currentTarget.textContent;
-    nav.textContent = lang;
-    e.currentTarget.classList.add('uk-active');
-  }
-  
-  select.forEach(node => {
-    node.addEventListener('click', handleClick)
-});
