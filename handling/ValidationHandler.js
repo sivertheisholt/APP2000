c5c50c4bda3480f2495fd74b147b666efef59baa
233518 <233518@usn.no>
2021-04-08 19:27:27 +0200

    Kommenterte ValidationHandler

--- handling/ValidationHandler.js
+++ handling/ValidationHandler.js
@@ -1,3 +1,7 @@
+/**
+ * ValidationHandler klassen brukes for å ha en felles return metode
+ * En klasse som inneholder status (Boolean) og informasjon (*)
+ */
 class ValidationHandler
 {
     constructor(status, information) {
