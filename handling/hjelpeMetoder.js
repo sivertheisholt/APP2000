bb46f4ef0fb08684b9176807c4b165ddbc9f2db5
233518 <233518@usn.no>
2021-04-08 19:09:55 +0200

    Kommenterte hjelpemetode funksjoner

--- handling/hjelpeMetoder.js
+++ handling/hjelpeMetoder.js
@@ -6,86 +6,130 @@ const ValidationHandler = require('./ValidationHandler');
 
 //Her kan dere legge inn hjelpemetoder dere vil lage
  var methods = {
-     //lagFinDato metoden gjør om en dato lettere å lese
-     lagFinDato: function(datoInn, stringTilSplitting) {
-         try {
+    /**
+     * lagFinDato metoden gjør en dato lettere å lese
+     * @param {Date} datoInn 
+     * @param {String} stringTilSplitting 
+     * @returns String
+     */
+    lagFinDato: function(datoInn, stringTilSplitting) {
+        try {
             let splitDato = datoInn.split(stringTilSplitting);
             const dato = new Date(splitDato[0], splitDato[1]-1, splitDato[2])
             return dato.toLocaleString('default', { day: 'numeric', month: 'long', year: 'numeric' });
-         } catch(err) {
-            logger.log({level: 'error', message: `Could not format date from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`}); 
-         }
-     },
-     lagFinDatoFraDB: function(datoInn, stringTilSplitting) {
-        try {
-           let d = datoInn.toLocaleString('default', { day: 'numeric', month: 'long', year: 'numeric' });
-           d = `${d.split(stringTilSplitting)[0]} ${d.split(stringTilSplitting)[1]}`
-           return d;
         } catch(err) {
-           logger.log({level: 'error', message: `Could not format date from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`}); 
+            logger.log({level: 'error', message: `Could not format date from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`}); 
         }
     },
-
-    // lagfinMåned Funksjon for å hente månedsnummer (plassering)
+    /**
+     * Formaterer dato fra database fin
+     * @param {Date} datoInn 
+     * @param {String} stringTilSplitting 
+     * @returns String
+     */
+    lagFinDatoFraDB: function(datoInn, stringTilSplitting) {
+        try {
+            let d = datoInn.toLocaleString('default', { day: 'numeric', month: 'long', year: 'numeric' });
+            d = `${d.split(stringTilSplitting)[0]} ${d.split(stringTilSplitting)[1]}`
+            return d;
+        } catch(err) {
+            logger.log({level: 'error', message: `Could not format date from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`}); 
+        }
+    },
+    /**
+     * Henter månedsnummer (plasser) fra dato
+     * @param {Date} datoInn 
+     * @param {String} stringTilSplitting 
+     * @returns Månedsnummer
+     */
     lagfinMåned: function(datoInn, stringTilSplitting) {
         try {
-                let splitDato = datoInn.split(stringTilSplitting);
-                const dato = new Date(splitDato[0], splitDato[1]-1, splitDato[2]-1)
-                const monthName = dato.getMonth()
-              return monthName
-            } catch(err) {
-                logger.log({level: 'error', message: `Could not format month from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`}); 
-            }
-        },
-
-    // lagfinDag Funksjon for å hente dagsnummer sin plassering
+            let splitDato = datoInn.split(stringTilSplitting);
+            const dato = new Date(splitDato[0], splitDato[1]-1, splitDato[2]-1)
+            const monthName = dato.getMonth()
+            return monthName
+        } catch(err) {
+            logger.log({level: 'error', message: `Could not format month from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`}); 
+        }
+    },
+    /**
+     * Henter dagsnummer sin plassering fra dato
+     * @param {Date} datoInn 
+     * @param {String} stringTilSplitting 
+     * @returns Dagsnummer
+     */
     lagfinDag: function(datoInn, stringTilSplitting) {
         try {
-                let splitDato = datoInn.split(stringTilSplitting);
-                const dato = new Date(splitDato[0], splitDato[1]-1, splitDato[2]-1)
-                const dagnavn = dato.getDate()
-                return dagnavn
-            } catch(err) {
-                logger.log({level: 'error', message: `Could not format day from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`});
-            }
-        },
-        
+            let splitDato = datoInn.split(stringTilSplitting);
+            const dato = new Date(splitDato[0], splitDato[1]-1, splitDato[2]-1)
+            const dagnavn = dato.getDate()
+            return dagnavn
+        } catch(err) {
+            logger.log({level: 'error', message: `Could not format day from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`});
+        }
+    },
     // lagfinÅrstall Funksjon Henter årstall
+    /**
+     * Henter årstall fra dato
+     * @param {Date} datoInn 
+     * @param {String} stringTilSplitting 
+     * @returns Årstall
+     */
     lagfinÅrstall: function(datoInn, stringTilSplitting) {
         try {
-                let splitDato = datoInn.split(stringTilSplitting);
-                const dato = new Date(splitDato[0], splitDato[1]-1, splitDato[2]-1)
-                const år = dato.getFullYear()
-                return år
-            } catch(err) {
-                logger.log({level: 'error', message: `Could not format year from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`});
-            }
-        },
-     validateEmail: function(email){
+            let splitDato = datoInn.split(stringTilSplitting);
+            const dato = new Date(splitDato[0], splitDato[1]-1, splitDato[2]-1)
+            const år = dato.getFullYear()
+            return år
+        } catch(err) {
+            logger.log({level: 'error', message: `Could not format year from ${datoInn} with string ${stringTilSplitting}! Error: ${err}`});
+        }
+    },
+    /**
+     * Sjekker om email er valid
+     * @param {String} email 
+     * @returns 
+     */
+    validateEmail: function(email){
         try{
             const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(String(email).toLowerCase());
-        }catch(err){
+        }catch(err) {
             logger.log({level: 'error', message: `Could not validate email ${email}! Error: ${err}`});
         }
-     },
-     validatePassword: function(password){
-         try{
+    },
+    /**
+     * Sjekker om passord er valid
+     * @param {String} password 
+     * @returns 
+     */
+    validatePassword: function(password){
+        try{
             const passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
             return passw.test(String(password));
-         }catch(err){
+        }catch(err) {
             logger.log({level: 'error', message: `Could not validate password! Error: ${err}`});
-         }
-     },
-     sjekkOmBildeLoader: function(url) {
+        }
+    },
+    /**
+     * Sjekker om bilde url laster inn
+     * @param {String} url 
+     * @returns boolean
+     */
+    sjekkOmBildeLoader: function(url) {
         return got(url, {http2: true}).then(res=>{
             if(res.statusCode == 200) {
                 return true;
             } else {
                 return false;
             }
-        })
+        })  
     },
+    /**
+     * Leser fil
+     * @param {String} path 
+     * @returns String
+     */
     lesFil: async function(path){
         return new Promise(function (resolve, reject) {
             fs.readFile(path, 'utf8', function (err, data) {
