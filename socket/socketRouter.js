7c02b2d047a0f10a427604c65b580a028a980ce6
233518 <233518@usn.no>
2021-04-10 22:57:57 +0200

    Forbedret koden

--- socket/socketRouter.js
+++ socket/socketRouter.js
@@ -1,50 +1,31 @@
-const logger = require("../logging/logger");
 const disconnect = require("./disconnect");
 const liveSearch = require("./liveSearch");
 const favourite = require("./favourite");
 const filter = require("./filter");
+
+const sockets = {
+    disconnect: disconnect,
+    userInputSearch: liveSearch,
+    favoriteMovie: favourite.addFavoriteMovie,
+    unFavoriteMovie: favourite.delFavoriteMovie,
+    favoriteTv: favourite.addFavoriteTv,
+    delFavoriteTv: favourite.delFavoriteTv,
+    popularMediaDesc: filter.popularMediaDesc,
+    popularMediaAsc: filter.popularMediaAsc,
+    popularMediaSortByDateDesc: filter.popularMediaSortByDateDesc,
+    popularMediaSortByDateAsc: filter.popularMediaSortByDateAsc,
+    popularMediaSortByTitleAZ: filter.popularMediaSortByTitleAZ,
+    popularMediaSortByTitleZA: filter.popularMediaSortByTitleZA,
+}
 
 /**
  * Fungerer som en "hub" - Sender videre til riktig fil/funksjon
  * @param {Object} socket 
  */
 function routeFunction(socket) {
-    socket.on('disconnect', () => {
-        disconnect(socket);
-    })
-    socket.on("userInputSearch", (userInputSearch) => {
-        liveSearch(socket, userInputSearch);
-    })
-    socket.on("favoriteMovie", (args) => {
-      favourite.addFavoriteMovie(socket, args);
-    });
-    socket.on('unFavoriteMovie', (args) => {
-      favourite.delFavoriteMovie(socket, args);
-    });
-    socket.on("favoriteTv", (args) => {
-      favourite.addFavoriteTv(socket, args);
-    });
-    socket.on('unfavoriteTv', (args) => {
-      favourite.delFavoriteTv(socket, args);
-    });
-    socket.on('popularMediaDesc', (args) => {
-      filter.popularMediaDesc(socket, args);
-    });
-    socket.on('popularMediaAsc', (args) => {
-      filter.popularMediaAsc(socket, args);
-    });
-    socket.on('popularMediaSortByDateDesc', (args) => {
-      filter.popularMediaSortByDateDesc(socket, args);
-    });
-    socket.on('popularMediaSortByDateAsc', (args) => {
-      filter.popularMediaSortByDateAsc(socket, args);
-    });
-    socket.on('popularMediaSortByTitleAZ', (args) => {
-      filter.popularMediaSortByTitleAZ(socket, args);
-    });
-    socket.on('popularMediaSortByTitleZA', (args) => {
-      filter.popularMediaSortByTitleZA(socket, args);
-    });
+    for(const key in sockets) {
+      socket.on(key, args => sockets[key](socket, args))
+    }
 }
 
 module.exports = routeFunction;
