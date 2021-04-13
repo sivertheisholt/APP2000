51c15fe75de78253ed8bfbd5eeae48444561caa3
233518 <233518@usn.no>
2021-04-13 14:52:32 +0200

    Fiks for admin knapp + sjekk om bruker logga inn

--- routing/dashboard.js
+++ routing/dashboard.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const asyncExpress = require('../handling/expressUtils');
+const checkUserMiddleware = require('../misc/express/middleware/checkUser');
 
 const userController = require('../controllers/userController');
 
-router.get("/dashboard", asyncExpress(userController.user_get_dashboard));
+router.get("/dashboard", checkUserMiddleware.user_check_loggedIn, asyncExpress(userController.user_get_dashboard));
 
-router.post("/dashboardChangePassword", asyncExpress(userController.user_post_changepassword));
+router.post("/dashboardChangePassword", checkUserMiddleware.user_check_loggedIn, asyncExpress(userController.user_post_changepassword));
 
-router.post("/changeUsername", asyncExpress(userController.user_post_changeusername));
+router.post("/changeUsername", checkUserMiddleware.user_check_loggedIn, asyncExpress(userController.user_post_changeusername));
 
-router.post('/upload-avatar', asyncExpress(userController.user_post_changeavatar));
+router.post('/upload-avatar', checkUserMiddleware.user_check_loggedIn, asyncExpress(userController.user_post_changeavatar));
 
 module.exports = router;
