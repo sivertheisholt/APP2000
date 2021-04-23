2bd1a4579f0cb079a442158e2525e9102554434b
233518 <233518@usn.no>
2021-04-23 19:22:30 +0200

    Fiksa splice is not a function

--- handling/recommendMovieHandler.js
+++ handling/recommendMovieHandler.js
@@ -10,8 +10,8 @@ const ValidationHandler = require('./ValidationHandler');
  */
 exports.recommendMovie = async function(user) {
     logger.log({level: 'debug', message: 'Creating recommended movies for user'})
-    if(!user || user.moviesWatched.length > 0) {
-        return new ValidationHandler(true, (await tmdb.data.getTrendingMovies()).splice(0, 10));
+    if(!user || user.moviesWatched.length < 0) {
+        return new ValidationHandler(true, (await tmdb.data.getTrendingMovies()).results.splice(0, 10));
     }
     const movies = await hjelpeMetoder.data.shuffleArray(getRecommendedMovies(user.moviesWatched));
     return new ValidationHandler(true, movies.splice(0,10));
@@ -23,8 +23,8 @@ exports.recommendMovie = async function(user) {
  */
 exports.recommendTv = async function(user) {
     logger.log({level: 'debug', message: 'Creating recommended movies for user'})
-    if(!user || user.tvsWatched.length > 0) {
-        return new ValidationHandler(true, (await tmdb.data.getTrendingTv()).splice(0, 10));
+    if(!user || user.tvsWatched.length < 0) {
+        return new ValidationHandler(true, (await tmdb.data.getTrendingTv()).results.splice(0, 10));
     }
     const tvs = await hjelpeMetoder.data.shuffleArray(getRecommendedTvs(user.tvsWatched));
     return new ValidationHandler(true, tvs.splice(0,10));
