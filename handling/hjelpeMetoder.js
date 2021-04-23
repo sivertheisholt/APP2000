91e7dd2a2e8af7c51f12f70ceb59067d4237091d
233518 <233518@usn.no>
2021-04-23 13:45:03 +0200

    Shuffle array funksjon

--- handling/hjelpeMetoder.js
+++ handling/hjelpeMetoder.js
@@ -216,6 +216,13 @@ const { stringify } = require('querystring');
             }
         }
         return new ValidationHandler(false, 'No matching language found');
+    },
+    shuffleArray: function(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
     }
  };
  
