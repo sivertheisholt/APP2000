e0489d97198677266a7cc605769c969dcf1f1cc9
dyb0 <dybevik2@gmail.com>
2021-04-28 12:09:12 +0200

    logging til aboutstats

--- misc/statistics/about_stats.js
+++ misc/statistics/about_stats.js
@@ -1,18 +1,22 @@
 const Movie = require('../database/filmSchema');
 const Tv = require('../database/tvSchema');
 const User = require('../database/brukerSchema');
+const logger = require('../../logging/logger');
 
 async function totalMovies() {
+    logger.log({level: 'debug', message: 'Getting total movies in database...'});
     let movieResult = await Movie.count();
     return movieResult.toString();
 }
 
 async function totalTvs() {
+    logger.log({level: 'debug', message: 'Getting total tv-shows in database...'});
     let tvResult = await Tv.count();
     return tvResult.toString();
 }
 
 async function totalUsers() {
+    logger.log({level: 'debug', message: 'Getting total registered users in database...'});
     let userResult = await User.count();
     return userResult.toString();
 }
