cc28477401b2375f3eb79cf5d2f55d35d3cf6d7f
233518 <233518@usn.no>
2021-05-20 16:08:47 +0200

    Lag ny liste i dashboard

---
+++ socket/list.js
@@ -0,0 +1,8 @@
+const listCreater = require('../systems/listSystem/listCreater');
+const userHandler = require('../handling/userHandler');
+
+exports.list_new = async function(socket, info) {
+    const userResult = await userHandler.getUserFromId(info.userId);
+    const result = await listCreater.createList(userResult.information, info.name);
+    socket.emit('newListResult', userResult.information.lists[userResult.information.lists.length]);
+}
