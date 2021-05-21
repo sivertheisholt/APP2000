23b8b9b1d1c58d729a2a47f03f5d40e089005780
dyb0 <dybevik2@gmail.com>
2021-05-22 01:16:05 +0200

    remove knapp favorites/watchlist fjerner med en gang

--- public/javascript/userdashboard/removemedia.js
+++ public/javascript/userdashboard/removemedia.js
@@ -1,32 +1,35 @@
 let favMediaCard = document.getElementById('mediaFavoriteCard');
 
-function removeMediaFav(id, type){
+function removeMediaFav(favId, type){
     switch(type){
         case 'movie':
-            socket.emit('unFavoriteMovie', id.toString());
+            socket.emit('unFavoriteMovie', favId.toString());
+            document.getElementById(favId+'mov').innerHTML = "";
+            document.getElementById(favId+'all').innerHTML = "";
             break;
         case 'tv':
-            socket.emit('delFavoriteTv', id.toString());
+            socket.emit('delFavoriteTv', favId.toString());
+            document.getElementById(favId+'tv').innerHTML = "";
+            document.getElementById(favId+'all').innerHTML = "";
             break;
         default:
             return;
     }
 }
 
-function removeMediaWatched(id, type){
+function removeMediaWatched(watchId, type){
     switch(type){
         case 'movie':
-            socket.emit('removeWatchedMovie', id.toString());
+            socket.emit('removeWatchedMovie', watchId.toString());
+            document.getElementById(watchId+'mov').innerHTML = "";
+            document.getElementById(watchId+'all').innerHTML = "";
             break;
         case 'tv':
-            socket.emit('removeWatchedTv', id.toString());
+            socket.emit('removeWatchedTv', watchId.toString());
+            document.getElementById(watchId+'tv').innerHTML = "";
+            document.getElementById(watchId+'all').innerHTML = "";
             break;
         default:
             return;
     }
 }
-
-
-/* socket.on('removedFavoriteResult', function(){
-    favMediaCard.style.display = 'none';
-}); */
