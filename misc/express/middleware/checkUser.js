34317dd9583468c028fb2a542746940062ad2f1e
233518 <233518@usn.no>
2021-05-21 15:38:42 +0200

    Husker språk

--- misc/express/middleware/checkUser.js
+++ misc/express/middleware/checkUser.js
@@ -3,10 +3,10 @@ const userHandler = require('../../../handling/userHandler');
 exports.user_check_admin = async function(req, res, next) {
     let userResult = await userHandler.getUser({_id: req.session.userId});
     if(!userResult.status) {
-        res.redirect('/');
+        res.redirect(`/${res.locals.currentLang}/homepage`);
     }
     if(!userResult.information.administrator){
-      res.redirect("/");
+      res.redirect(`/${res.locals.currentLang}/homepage`);
     }
     next();
 }
@@ -14,7 +14,7 @@ exports.user_check_admin = async function(req, res, next) {
 exports.user_check_loggedIn = async function(req, res, next) {
     let userResult = await userHandler.getUser({_id: req.session.userId});
     if(!userResult.status) {
-        res.redirect('/');
+        res.redirect(`/${res.locals.currentLang}/homepage`);
     }
     next();
 }
