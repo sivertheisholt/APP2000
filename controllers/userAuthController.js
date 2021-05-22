1a8f6b40515961b4ee9055a5fb018eb2921f0d61
233518 <233518@usn.no>
2021-05-22 18:40:27 +0200

    Forbedret user auth koden

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -1,22 +1,22 @@
 const Bruker = require('../database/brukerSchema');
 const bcrypt = require("bcrypt");
 const hjelpeMetoder = require('../handling/hjelpeMetoder');
-var mailer = require('../handling/mailer');
+let mailer = require('../handling/mailer');
 const jwt = require('jsonwebtoken');
 const logger = require('../logging/logger');
+const userHandler = require('../handling/userHandler');
 
 exports.userAuth_get_logout = async function(req, res) {
     logger.log({level: 'debug', message: `Request received for /logout`}); 
     req.session.destroy(err => {
         res.clearCookie('connect.sid')
-        res.redirect(`/${res.locals.currentLang}/homepage`)
+        res.redirect(`/${req.renderObject.langCode}/homepage`)
     })
 }
 
 exports.userAuth_get_resetpassword = async function(req, res) {
     logger.log({level: 'debug', message: `Request received for /resetpassword/:token`}); 
-    let token = req.params.token;
-    req.renderObject.token = token;
+    req.renderObject.token = req.params.token;
     res.render("auth/resetpassword", req.renderObject);
 }
 
@@ -32,34 +32,42 @@ exports.userAuth_get_resetpassword_error = async function(req, res) {
 
 exports.userAuth_post_signup = async function(req,res ) {
     logger.log({level: 'debug', message: `Request received for /signup`}); 
-    const pugBody = req.body; //Skaffer body fra form
+
+    //Skaffer body fra form
+    const pugBody = req.body;
+
     //Sjekker at mail tilfredsstiller krav
     if(!(hjelpeMetoder.data.validateEmail(pugBody.email))){
         logger.log({level: 'debug', message: `Email ${pugBody.email} is not properly formatted!`}); 
-        res.redirect(`/${res.locals.currentLang}/homepage?error=Email is not properly formatted&errorType=signup`);
+        res.redirect(`/${req.renderObject.langCode}}/homepage?error=Email is not properly formatted&errorType=signup`);
         return;
     }
-    if(await Bruker.findOne({email: pugBody.email})) {
+
+    //Sjekker om epost er tatt
+    if((await userHandler.getUserFromEmail(pugBody.email)).status) {
         logger.log({level: 'debug', message: `Email ${pugBody.email} is already taken!`});
-        res.redirect(`/${res.locals.currentLang}/homepage?error=Email is already taken&errorType=signup`);
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=Email is already taken&errorType=signup`);
         return;
     }
+
     //Sjekker at passord tilfredstiller krav
     if(!(hjelpeMetoder.data.validatePassword(pugBody.password))){
         logger.log({level: 'debug', message: `Password is not properly formatted!`}); 
-        res.redirect(`/${res.locals.currentLang}/homepage?error=Password is not properly formatted&errorType=signup`);
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=Password is not properly formatted&errorType=signup`);
         return;
     }
+
     //Vi gjør en sjekk at alle feltene er fylt inn
     if(!(pugBody.email && pugBody.password && pugBody.passwordRepeat)) {
         logger.log({level: 'debug', message: `All form inputs are not filled!`}); 
-        res.redirect(`/${res.locals.currentLang}/homepage?error=Data is not properly formatted&errorType=signup`);
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=Data is not properly formatted&errorType=signup`);
         return;
     }
+
     //Vi gjør en sjekk at passord 1 er lik passord 2 (Repeat password)
     if(!(pugBody.password == pugBody.passwordRepeat)) {
         logger.log({level: 'debug', message: `Passwords do not match each other!`}); 
-        res.redirect(`/${res.locals.currentLang}/homepage?error=Passwords do not match&errorType=signup`);
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=Passwords do not match&errorType=signup`);
         return;
     }
 
@@ -67,134 +75,154 @@ exports.userAuth_post_signup = async function(req,res ) {
     const bruker = new Bruker(pugBody);
     
     //Nå må vi lage ny salt for å hashe passord
-    const salt = await bcrypt.genSalt(10); //Her kommer await (Se async) inn (Nå venter vi til bcrypt er ferdig)
+    const salt = await bcrypt.genSalt(10);
 
     //Nå setter vi passord til det hasha passordet
     bruker.password = await bcrypt.hash(bruker.password, salt);
-    bruker.save().then((dokument, err) => {
-        if(err) {
-            logger.log({level: 'error', message: `Could not save user to database! Error: ${err}`});
-            return;
-        }
-        logger.log({level: 'debug', message: `Saving user to database`}); 
-        mailer({
-            from: process.env.EMAIL,
-            to: bruker.email, //bruker.email skal brukes her når det skal testes mot "ekte" bruker,
-            subject: 'Welcome to Filmatory!',
-            html: `<h1>Hope you enjoy your time at Filmatory!</h1>`
-        });
-    })
-    res.redirect(`/${res.locals.currentLang}/homepage`);
+
+    //Lagrer bruker i databasen
+    const userResult = await userHandler.newUser(bruker);
+    if(!userResult.status) {
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=Something unexpected happen when trying to create your user!&errorType=signup`);
+        return;
+    }
+
+    //Sender mail til bruker
+    mailer({
+        from: process.env.EMAIL,
+        to: bruker.email, //bruker.email skal brukes her når det skal testes mot "ekte" bruker,
+        subject: 'Welcome to Filmatory!',
+        html: `<h1>Hope you enjoy your time at Filmatory!</h1>`
+    });
+
+    //Suksess
+    res.redirect(`/${req.renderObject.langCode}/homepage`);
 }
 
-exports.userAuth_post_forgottenPassword = function(req, res) {
+exports.userAuth_post_forgottenPassword = async function(req, res) {
     logger.log({level: 'debug', message: `Request received for /forgottenPassword`}); 
     const pugBody = req.body;
-    Bruker.findOne({email: pugBody.emailForgottenPassword}, (err, bruker) => {
-        if(!bruker) {
-            logger.log({level: 'error', message: `User with email ${pugBody.emailForgottenPassword} does not exist`}); 
-            res.redirect(`/${res.locals.currentLang}/homepage?error=User with this email does not exist&errorType=forgottenPassword`);
-            return;
-        }
-        if(err) {
-            logger.log({level: 'error', message: `Something went wrong when finding user! Error: ${err}`});
-            res.redirect(`/${res.locals.currentLang}/homepage?error=Something went wrong&errorType=forgottenPassword`);
-        }
-        const token = jwt.sign({_id: bruker._id}, process.env.RESET_PASSWORD_KEY, {expiresIn:'60m'});
-        return bruker.updateOne({resetLink: token}, function(err) {
-            if(err) {
-                logger.log({level: 'error', message: `Something unexpected happen! Error: ${err}`}); 
-                res.redirect(`/${res.locals.currentLang}/homepage?error=Reset password link error&errorType=forgottenPassword`);
-                return;
-            } else {
-                let link = `${req.renderObject.url}/${req.renderObject.urlPath}/auth/resetpassword/${token}`
-                logger.log({level: 'debug', message: `Link ${link} sent`}); 
-                mailer({
-                    from: process.env.EMAIL,
-                    to: bruker.email,
-                    subject: 'Password Reset Link',
-                    html: `
-                    <h2>Please click on the link below reset your password</h2>
-                    <p>This link will expire in 60 minutes</p>
-                    <p>${link}</p>
-                    `
-                });
-                res.redirect(`/${res.locals.currentLang}/homepage`);
-            }
-        })
-    })
+
+    //Skaffer bruker
+    const userResult = await userHandler.getUserFromEmail(pugBody.emailForgottenPassword);
+    if(!userResult.status) {
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=User with this email does not exist&errorType=forgottenPassword`);
+        return;
+    }
+
+    //Lager token og oppdaterer bruker
+    const token = jwt.sign({_id: userResult.information._id}, process.env.RESET_PASSWORD_KEY, {expiresIn:'60m'});
+    const updateUser = await userHandler.updateUser(userResult.information, {resetLink: token});
+    if(!updateUser.status) {
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=Reset password link error&errorType=forgottenPassword`);
+        return;
+    }
+    
+    //Lager link
+    let link = `${req.renderObject.url}/${req.renderObject.urlPath}/auth/resetpassword/${token}`
+    logger.log({level: 'debug', message: `Link ${link} sent`}); 
+    
+    //Sender mail
+    mailer({
+        from: process.env.EMAIL,
+        to: userResult.information.email,
+        subject: 'Password Reset Link',
+        html: `
+        <h2>Please click on the link below reset your password</h2>
+        <p>This link will expire in 60 minutes</p>
+        <p>${link}</p>
+        `
+    });
+
+    //Suksess
+    res.redirect(`/${req.renderObject.langCode}/homepage`);
 }
 
 exports.userAuth_get_login = async function(req,res ) {
     logger.log({level: 'debug', message: `Request received for /login`}); 
-    const pugBody = req.body; //Skaffer body fra form
-    const bruker = await Bruker.findOne({email: pugBody.email}); //Finner brukeren fra databasen
-    //Nå skal vi sjekke om passordet stemmer
-    if(bruker) {
-        const sjekkPassword = await bcrypt.compare(pugBody.password, bruker.password); //Bruker bcrypt for å sammenligne, true/false return
-        if(bruker.banned){
-            res.redirect(`/${res.locals.currentLang}/homepage?error=This user is banned&errorType=login`);
-        }
-        if (sjekkPassword) {
-            logger.log({level: 'debug', message: `Setting session`}); 
-            req.session.userId = bruker._id; //Setter session
-            res.redirect(`/${res.locals.currentLang}/homepage`);
-            return;
-        } else {
-            logger.log({level: 'debug', message: `Invalid password for ${bruker._id}`}); 
-            res.redirect(`/${res.locals.currentLang}/homepage?error=Invalid Password&errorType=login`);
-            return;
-        }
-    } else {
-        logger.log({level: 'debug', message: `User does not exist`}); 
-        res.redirect(`/${res.locals.currentLang}/homepage?error=User does not exist&errorType=login`);
+
+    //Skaffer body fra form
+    const pugBody = req.body;
+
+    //Skaffer bruker
+    const userResult = await userHandler.getUserFromEmail(pugBody.email);
+    if(!userResult.status) {
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=User does not exist&errorType=login`);
         return;
     }
+
+    //Sjekker om bruker er banna
+    if(userResult.information.banned) {
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=This user is banned&errorType=login`);
+        return;
+    }
+
+    //Sjekker passord
+    const sjekkPassword = await bcrypt.compare(pugBody.password, userResult.information.password);
+    if(!sjekkPassword) {
+        logger.log({level: 'debug', message: `Invalid password for ${userResult.information._id}`}); 
+        res.redirect(`/${req.renderObject.langCode}/homepage?error=Invalid Password&errorType=login`);
+        return;
+    }
+
+    //Setter session
+    logger.log({level: 'debug', message: `Setting session`});
+    req.session.userId = userResult.information._id;
+
+    //Suksess
+    res.redirect(`/${req.renderObject.langCode}/homepage`);
 }
 
-exports.userAuth_post_resetpassword = function(req, res) {
+exports.userAuth_post_resetpassword = async function(req, res) {
     logger.log({level: 'debug', message: `Request received for /resetPassword/:token`}); 
+
+    //Skaffer link og info
     const resetLink = req.params.token;
     const pugBody = req.body;
-    if(resetLink){
-        jwt.verify(resetLink, process.env.RESET_PASSWORD_KEY, function(err, decodedData) {
-            if(err) {
-                logger.log({level: 'error', message: `Incorrect token or it has expired`});
-                return res.redirect(`/${res.locals.currentLang}/auth/resetpassworderror`);
-            }
-            Bruker.findOne({resetLink}, async (err, bruker) => {
-                if(err) {
-                    logger.log({level: 'error', message: `Something unexpected happen when trying to find user! Error: ${err}`});
-                    return res.redirect(`/${res.locals.currentLang}/auth/resetpassword/${resetLink}?error=Unexpected error when trying to find user&errorType=resetPassword`);
-                }
-                if(!bruker) {
-                    logger.log({level: 'error', message: `User with this token does not exist`});
-                    return res.redirect(`/${res.locals.currentLang}/auth/resetpassword/${resetLink}?error=User with this token does not exist&errorType=resetPassword`);
-                }
-                if(!(pugBody.newPassword == pugBody.newPasswordRepeat)) {
-                    logger.log({level: 'debug', message: `Passwords do not match`});
-                    return res.redirect(`/${res.locals.currentLang}/auth/resetpassword/${resetLink}?error=Passwords do not match&errorType=resetPassword`);
-                }
-                //Nå må vi lage ny salt for å hashe passord
-                const salt = await bcrypt.genSalt(10); //Her kommer await (Se async) inn (Nå venter vi til bcrypt er ferdig)
 
-                //Nå setter vi passord til det hasha passordet
-                bruker.password = await bcrypt.hash(pugBody.newPassword, salt);
-                bruker.resetLink = '';
-
-                bruker.save((err, result) => {
-                    if(err) {
-                        logger.log({level: 'error', message: `Something unexpected happen when trying to save user to database! Error: ${err}`});
-                        return res.redirect(`/${res.locals.currentLang}/auth/resetpassword/${resetLink}?error=Could not change password&errorType=resetPassword`);
-                    } else {
-                        logger.log({level: 'debug', message: `Password successfully changed for user`});
-                        return res.redirect(`/${res.locals.currentLang}/auth/resetpasswordsuccess`);
-                    }
-                })
-            })
-        })
-    }else {
+    //Sjekker om link eksisterer
+    if(!resetLink){
         logger.log({level: 'error', message: `Authentication error`});
-        return res.status(401).json({error: 'Authentication error!!!'});
+        return res.status(401).json({error: 'Authentication error!'});
     }
+
+    //Sjekker info
+    jwt.verify(resetLink, process.env.RESET_PASSWORD_KEY, async function(err, decodedData) {
+        if(err) {
+            logger.log({level: 'error', message: `Incorrect token or it has expired`});
+            return res.redirect(`/${req.renderObject.langCode}/auth/resetpassworderror`);
+        }
+
+        //Skaffer bruker
+        const userResult = await userHandler.getUser({resetLink})
+        if(!userResult.status) {
+            res.redirect(`/${req.renderObject.langCode}/auth/resetpassword/${resetLink}?error=User with this token does not exist&errorType=resetPassword`);
+            return;
+        }
+
+        //Sjekker om felt er like
+        if(!(pugBody.newPassword == pugBody.newPasswordRepeat)) {
+            logger.log({level: 'debug', message: `Passwords do not match`});
+            res.redirect(`/${req.renderObject.langCode}/auth/resetpassword/${resetLink}?error=Passwords do not match&errorType=resetPassword`);
+            return;
+        }
+
+        //Nå må vi lage ny salt for å hashe passord
+        const salt = await bcrypt.genSalt(10);
+
+        //Nå setter vi passord til det hasha passordet
+        userResult.information.password = await bcrypt.hash(pugBody.newPassword, salt);
+        userResult.information.resetLink = '';
+
+        //Lagrer bruker
+        const updateUser = await userHandler.updateUser(userResult.information);
+        if(!updateUser.status) {
+            res.redirect(`/${req.renderObject.langCode}/auth/resetpassword/${resetLink}?error=Could not change password&errorType=resetPassword`);
+            return;
+        }
+
+        //Suksess
+        logger.log({level: 'debug', message: `Password successfully changed for user`});
+        res.redirect(`/${req.renderObject.langCode}/auth/resetpasswordsuccess`);
+    })
 }
--- handling/userHandler.js
+++ handling/userHandler.js
@@ -38,11 +38,11 @@ async function getUserFromId(userId) {
  * @returns ValidationHandler
  */
 async function getUserFromEmail(userEmail) {
-    logger.log({level: 'debug', message: `Getting user with id ${userEmail} from database`})
+    logger.log({level: 'debug', message: `Getting user with email ${userEmail} from database`})
     const user = await Bruker.findOne({email: userEmail});
     if(!user) {
-        logger.log({level: 'error', message: `User with id ${userEmail} was not found`}); 
-        return new ValidationHandler(false, `Can't find user with id ${userEmail} in database`);
+        logger.log({level: 'error', message: `User with email ${userEmail} was not found`}); 
+        return new ValidationHandler(false, `Can't find user with email ${userEmail} in database`);
     }
     return new ValidationHandler(true, user);
 }
@@ -57,7 +57,7 @@ async function updateUser(user, options) {
     logger.log({level: 'debug', message: `Updating user ${user._id} with ${options}`});
     return user.updateOne(options).then((doc, err) => {
         if(err) {
-            logger.log({level: 'error', message: `There was an error updating user with options ${options}! Error: ${err}`});
+            logger.log({level: 'error', message: `There was an error updating user with options ${options}! ${err}`});
             return new ValidationHandler(false, 'Could not update user');
         }
         logger.log({level: 'info', message: `User with id ${user._id} was successfully updated with options ${options}`});
@@ -67,15 +67,15 @@ async function updateUser(user, options) {
 
 /**
  * Skaffer objekter fra bruker fra ID
- * @param {String} userId 
+ * @param {String} userId
  * @param {String} fields 
  * @returns ValidationHandler
  */
-async function getFieldsFromUserById(userId, fields) {
+function getFieldsFromUserById(userId, fields) {
     logger.log({level: 'debug', message: `Getting user ${userId} with ${fields}`});
     return Bruker.findOne({_id: userId}).select(fields).then((doc, err) => {
         if(err) {
-            logger.log({level: 'error', message: `There was an error getting user with fields ${fields}! Error: ${err}`});
+            logger.log({level: 'error', message: `There was an error getting user with fields ${fields}! ${err}`});
             return new ValidationHandler(false, 'Could not retrieve user fields');
         }
         logger.log({level: 'info', message: `User with id ${userId} was successfully found with ${fields}`});
@@ -83,4 +83,22 @@ async function getFieldsFromUserById(userId, fields) {
     });
 }
 
-module.exports = {getUser, getUserFromId,getUserFromEmail, updateUser, getFieldsFromUserById}
+/**
+ * Lager en ny bruker i databasen
+ * @param {Object} user 
+ * @author Sivert - 233518
+ */
+function newUser(user) {
+    logger.log({level: 'info', message: `Creating new user in database`});
+    user.save().then((doc, err) => {
+        if(err) {
+            logger.log({level: 'error', message: `Could not save user to database! ${err}`});
+            return new ValidationHandler(false, 'Could not save user to database');
+        }
+    })
+    return new ValidationHandler(true, 'User was successfully created');
+}
+
+
+
+module.exports = {getUser, getUserFromId,getUserFromEmail, updateUser, getFieldsFromUserById, newUser}
