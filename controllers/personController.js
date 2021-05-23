1aa7b121737d53f6c6d6e1523f9b0e5398eef354
233518 <233518@usn.no>
2021-05-23 21:05:33 +0200

    OVersettelse fiks for personer

--- controllers/personController.js
+++ controllers/personController.js
@@ -4,13 +4,18 @@ const logger = require('../logging/logger');
 
 exports.personInfo_get = async function (req, res) {
     logger.log({level: 'debug' ,message:'Finding session for user'})
+    const personId = req.url.slice(1);
     //Lager person objekt
     let person = {
-      personinfo: await tmdb.data.getPersonByID(req.url.slice(1), req.renderObject.urlPath),
-      links: await tmdb.data.getPersonLinksByID(req.url.slice(1), req.renderObject.urlPath)
+      personinfo: await tmdb.data.getPersonByID(personId, req.renderObject.urlPath),
+      links: await tmdb.data.getPersonLinksByID(personId, req.renderObject.urlPath)
     }
 
-    req.renderObject.credits = await tmdb.data.getPersonCombinedCreditsByID(req.url.slice(1), req.renderObject.urlPath);
+    if(person.personinfo.biography == "" || !person.personinfo.biography) {
+      person.personinfo = await tmdb.data.getPersonByID(personId, 'en')
+    }
+
+    req.renderObject.credits = await tmdb.data.getPersonCombinedCreditsByID(personId, req.renderObject.urlPath);
     req.renderObject.person = person;
     res.render("person/personinfo", req.renderObject);
 }
