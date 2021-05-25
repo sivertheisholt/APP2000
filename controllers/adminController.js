641a638e7513954fe3ba46ceb9908b8fc9ebcbe9
dyb0 <dybevik2@gmail.com>
2021-05-25 02:31:28 +0200

    error meldinger + oversettelser i admindash

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const hjelpemetoder = require('../handling/hjelpeMetoder');
 const fs = require("fs");
 const reviewGetter = require('../systems/reviewSystem/reviewGetter');
 const ticketGetter = require('../systems/ticketSystem/ticketGetter');
+const logger = require('../logging/logger');
 
 /**
  * Get for admindashbordet, laster inn anmeldelser og tickets i påvente av svar og godkjenning
@@ -42,26 +43,28 @@ exports.admin_get_dashboard = async function(req, res) {
 /**
  * Post for å legge til nytt språk til nettsiden, den lager en ny JSON fil med språkkoden som er skrevet inn.
  * JSON filen er kopiert fra den engelske, slik kan administrator oversette fra engelsk til ønsket språk.
- * @param {Object} req 
+ * @param {Object} req Forespørsel fra klient
  * @param {Object} res Respons fra server
  * @returns 
  * @author Ørjan Dybevik - 233530
  */
 exports.admin_post_addlanguage = async function(req, res) {
-  const pugBody = req.body; //Skaffer body fra form
+  const pugBody = req.body.admin_add_lang_details; //Skaffer body fra form
   let languageJson = await hjelpemetoder.data.lesFil("./lang/langList.json");
   let langs = JSON.parse(languageJson.information);
-  let langObj = hjelpemetoder.data.validateNewLang(pugBody);
-
+  let langObj = hjelpemetoder.data.validateNewLang(pugBody, req);
 
   if(!langObj.status){
-    return res.redirect(`/${res.locals.currentLang}/admin/admindashboard?error=${langObj.information}&errorType=adminDashboardCreateLanguage`);
+    logger.log({level: 'debug', message: 'Error when validating language to add'});
+    return res.status(400).send({error: `${langObj.information}`});
   }
   langs.availableLanguage.push(langObj.information);
   languageJson = JSON.stringify(langs);
   fs.writeFileSync("./lang/langList.json", languageJson, "utf-8");
   fs.copyFileSync("./lang/en.json", `./lang/${langObj.information.id}.json`);
-  return res.redirect(`/${res.locals.currentLang}/admin/admindashboard`);
+
+  logger.log({level: 'debug', message: `Successfully added new language`});
+  res.status(200).send({message: req.__('SUCCESS_LANGUAGE_ADDED')});
 }
 
 /**
@@ -73,15 +76,18 @@ exports.admin_post_addlanguage = async function(req, res) {
  * @author Ørjan Dybevik - 233530
  */
 exports.admin_post_deletelanguage = async function(req, res) {
-  const langToDelete = req.body.admindashlangdelete;
-  let langObj = await hjelpemetoder.data.validateDeleteLang(langToDelete);
+  const langToDelete = req.body.admin_delete_lang_details;
+  let langObj = await hjelpemetoder.data.validateDeleteLang(langToDelete.admindashlangdelete, req);
   if(!langObj.status){
-    return res.redirect(`/${res.locals.currentLang}/admin/admindashboard?error=${langObj.information}&errorType=adminDashboardDeleteLanguage`);
+    logger.log({level: 'error', message: 'Error when validating language to delete'});
+    return res.status(400).send({error: `${langObj.information}`});
   }
   fs.unlinkSync(`./lang/${langObj.information.id}.json`, (err) => {
     if(err){
-      return res.redirect(`/${res.locals.currentLang}/admin/admindashboard?error=could not delete old language&errorType=adminDashboardDeleteLanguage`);
+      logger.log({level: 'debug', message: 'Could not delete old language'});
+      return res.status(400).send({error: req.__('ERROR_DELETE_LANGUAGE')});
     }
   });
-  return res.redirect(`/${res.locals.currentLang}/admin/admindashboard`);
+  logger.log({level: 'debug', message: `Successfully deleted the language`});
+  res.status(200).send({message: req.__('SUCCESS_LANGUAGE_DELETED')});
 }
