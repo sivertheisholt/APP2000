8396eaaa20dabb9b30f1ba94410fe8a5ad7ecbfa
Sigve E. Eliassen <sigve888@gmail.com>
2021-10-28 01:54:22 +0200

    Skriveleif i routing api serie watchproviders

--- routing/api.js
+++ routing/api.js
@@ -75,7 +75,7 @@ router.route("/tv/upcomingtvs")
 router.route("/tv/tvs")
     .get(asyncExpress(apiController.tv_get_tvs));
 
-router.route("tv/get/watch/providers/:tvId")
+router.route("/tv/get/watch/providers/:tvId")
     .get(asyncExpress(apiController.tv_get_watch_providers))
 
 
